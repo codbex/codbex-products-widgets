f6f6101b667cb861caad6c5ccbc8a098a3540fa4
bob4o-afk <bobkatasad@abv.bg>
2024-08-16 15:18:54 +0300

    fix: hard reset

--- codbex-products-widgets/js/inventory-accuracy.js
+++
@@ -1,5 +0,0 @@
-const dashboard = angular.module('inventoryAccuracy', []);
-
-dashboard.controller('InventoryAccuracyController', ['$scope', function ($scope) {
-    $scope.today = new Date();
-}]);
